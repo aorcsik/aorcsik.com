a58c2989b596e0f22da0e629711076ecba281c7f
Antal Orcsik <aorcsik@gmail.com>
2017-03-27 22:34:14 +0200

    shallow cloning dist

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ npm install
 
 mkdir ./dist
 
-git clone git@github.com:aorcsik/aorcsik.com.git --branch gh-pages dist
+git clone --depth 1 git@github.com:aorcsik/aorcsik.com.git --branch gh-pages dist
--- src/md/test.md
+++ src/md/test.md
@@ -1,1 +1,3 @@
 **Hello World!**
+
+This is much better!
