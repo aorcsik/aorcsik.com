afc5e7195e57a8f3189893b4bbb632e480465bf0
Antal Orcsik <aorcsik@hu.ibm.com>
2017-05-29 23:35:32 +0200

    native code + palette

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
+require('smoothscroll-polyfill').polyfill();
 
 import "../less/index.less";
+
+window.addEventListener("load", function(event) {
+    Array.from(document.querySelectorAll("a[href^='#']"), element => {
+        element.addEventListener("click", event => {
+            var targetId = element.href.split("#")[1];
+            if (targetId !== undefined) {
+                event.preventDefault();
+                if (targetId == "") {
+                    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+                } else {
+                    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+                }
+            }
+        });
+    });
+});
