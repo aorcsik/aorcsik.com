4dae2405f787c25d94d9ff80e0069c9e587bb818
Antal Orcsik <aorcsik@hu.ibm.com>
2017-06-03 02:25:21 +0200

    fix domain

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ git clone --depth 1 git@github.com:aorcsik/aorcsik.com.git --branch gh-pages dis
 
 ./build.sh -p
 
+cp CNAME ./dist/CNAME
+
 cd dist
 git add --all
 git commit -m "Release at $(date)"
