01fc3dc2f949dbc3188e42cbb80b61d38a2540a6
Antal Orcsik <aorcsik@hu.ibm.com>
2017-06-08 10:07:00 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 rm -rf ./dist
-cp -R ./public ./dist
+
 git clone --depth 1 git@github.com:aorcsik/aorcsik.com.git --branch gh-pages dist
 
 ./build.sh -p
 
 cp CNAME ./dist/CNAME
+cp -R ./public/.well-known ./dist/
 
 cd dist
 git add --all
